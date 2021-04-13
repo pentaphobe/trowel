21aeebf28f99eebb00e9e3c48c26d271c463ffa7
Keilin Olsen <pentaphobe@users.noreply.github.com>
2021-04-13 18:07:26 +1000

    Update example

--- example/example.go
+++ example/example.go
@@ -10,7 +10,7 @@ const exampleJSON = `{
   "foo": {
     "baz": [1, "boffle", false, {
       "bling": true
-    }]	
+    }]
   },
 	"key.with.special_characters": true
 }
@@ -23,7 +23,7 @@ func main() {
 		panic(err)
 	}
 
-	// Wrap the object
+	// Wrap the object in a Trowel cursor
 	t := trowel.NewTrowel(obj)
 
 	result, err := t.Path(`.foo.baz[3].bling`)
@@ -32,7 +32,22 @@ func main() {
 	}
 	println(result.Get().(bool))
 
+	// equivalent to above
+	result, err = t.Key("foo").Key("baz").Index(3).Key("bling")
+	if err != nil {
+		panic(err)
+	}
+	println(result.Get().(bool))
+
+	// quoted keys
 	result, err = t.Path(`."key.with.special_characters"`)
+	if err != nil {
+		panic(err)
+	}
+	println(result.Get().(bool))
+
+	// invalid access also returns a cursor, so chaining is safe
+	result, err = t.Key("foo").Key("NO-OP").Index(3000).Key("bling")
 	if err != nil {
 		panic(err)
 	}
