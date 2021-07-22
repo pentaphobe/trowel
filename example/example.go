f91165bd5f704a837d610fa323a5e7d62cd20b54
Keilin Olsen <keilin@vectorflip.com>
2021-07-23 04:08:20 +1000

    Revert to not returning errors
    Adding error returns broke chaining
    
    The library now attempts to store error context in itself, providing some helper functions

--- example/example.go
+++ example/example.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/pentaphobe/trowel"
 )
@@ -17,6 +18,11 @@ const exampleJSON = `{
 `
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recovered:", r)
+		}
+	}()
 	var obj interface{}
 	err := json.Unmarshal([]byte(exampleJSON), &obj)
 	if err != nil {
@@ -26,30 +32,30 @@ func main() {
 	// Wrap the object in a Trowel cursor
 	t := trowel.NewTrowel(obj)
 
-	result, err := t.Path(`.foo.baz[3].bling`)
-	if err != nil {
-		panic(err)
+	result := t.Path(`.foo.baz[3].bling`)
+	if result.HasErrors() {
+		panic(result.Error())
 	}
 	println(result.Get().(bool))
 
 	// equivalent to above
-	result, err = t.Key("foo").Key("baz").Index(3).Key("bling")
-	if err != nil {
-		panic(err)
+	result = t.Key("foo").Key("baz").Index(3).Key("bling")
+	if result.HasErrors() {
+		panic(result.Error())
 	}
 	println(result.Get().(bool))
 
 	// quoted keys
-	result, err = t.Path(`."key.with.special_characters"`)
-	if err != nil {
-		panic(err)
+	result = t.Path(`."key.with.special_characters"`)
+	if result.HasErrors() {
+		panic(result.Error())
 	}
 	println(result.Get().(bool))
 
 	// invalid access also returns a cursor, so chaining is safe
-	result, err = t.Key("foo").Key("NO-OP").Index(3000).Key("bling")
-	if err != nil {
-		panic(err)
+	result = t.Key("foo").Key("NO-OP").Index(3000).Key("bling")
+	if result.HasErrors() {
+		panic(result.Error())
 	}
 	println(result.Get().(bool))
 }
--- trowel.go
+++ trowel.go
@@ -8,11 +8,11 @@ import (
 
 type Trowel interface {
 	// Get value from underlying array
-	Index(idx int) (Trowel, error)
+	Index(idx int) Trowel
 	// Get value from underlying dictionary
-	Key(key string) (Trowel, error)
+	Key(key string) Trowel
 	// Get by a path string
-	Path(path string) (Trowel, error)
+	Path(path string) Trowel
 
 	// Get underlying data
 	Get() interface{}
@@ -20,46 +20,57 @@ type Trowel interface {
 	Array() ([]interface{}, error)
 	// Get underlying data as a dictionary
 	Map() (map[string]interface{}, error)
+
+	// Get underlying errors
+	HasErrors() bool
+	Errors() []error
+	Error() error
+	addError(...error) Trowel
 }
 
 type trowelWrapper struct {
-	data interface{}
+	data   interface{}
+	errors []error
 }
 
 func (w *trowelWrapper) Get() interface{} {
 	return w.data
 }
 
-func (w *trowelWrapper) Index(idx int) (Trowel, error) {
+func (w *trowelWrapper) Index(idx int) Trowel {
 	if w.data == nil {
-		return &trowelWrapper{}, NewIndexError("can't do index lookup (%d) on nil", idx)
+		w.addError(NewIndexError(w, "can't do index lookup (%d) on nil", idx))
+		return w
 	}
 	var arr []interface{}
 	var ok bool
 	if arr, ok = w.data.([]interface{}); !ok {
-		return &trowelWrapper{}, NewIndexError("can't do index lookup (%d) on non-array %v", idx, w.data)
+		return NewTrowel(nil).addError(NewIndexError(w, "can't do index lookup (%d) on non-array %v", idx, w.data))
 	}
 	return &trowelWrapper{
-		data: arr[idx],
-	}, nil
+		data:   arr[idx],
+		errors: w.errors,
+	}
 }
 
-func (w *trowelWrapper) Key(key string) (Trowel, error) {
+func (w *trowelWrapper) Key(key string) Trowel {
 	if w.data == nil {
-		return &trowelWrapper{}, NewKeyError("can't do key lookup (%s) on nil", key)
+		w.addError(NewKeyError(w, "can't do key lookup (%s) on nil", key))
+		return w
 	}
 	var mp map[string]interface{}
 	var ok bool
 	if mp, ok = w.data.(map[string]interface{}); !ok {
-		return &trowelWrapper{}, NewKeyError("can't do key lookup (%s) on non-dictionary %v", key, w.data)
+		return NewTrowel(nil).addError(NewKeyError(w, "can't do key lookup (%s) on non-dictionary %v", key, w.data))
 	}
 	var value interface{}
 	if value, ok = mp[key]; !ok {
-		return &trowelWrapper{}, NewKeyError("no key matching '%s'", key)
+		return NewTrowel(nil).addError(NewKeyError(w, "no key matching '%s'", key))
 	}
 	return &trowelWrapper{
-		data: value,
-	}, nil
+		data:   value,
+		errors: w.errors,
+	}
 }
 
 func parsePath(pathStr string) ([]interface{}, error) {
@@ -100,52 +111,74 @@ func parsePath(pathStr string) ([]interface{}, error) {
 	return result, nil
 }
 
-func (w *trowelWrapper) Path(path string) (Trowel, error) {
+func (w *trowelWrapper) Path(path string) Trowel {
 	pathComponents, err := parsePath(path)
 	if err != nil {
-		return nil, err
+		return NewTrowel(nil).addError(err)
 	}
 	var child Trowel = w
 	for _, component := range pathComponents {
 		switch v := component.(type) {
 		case int:
-			child, err = child.Index(v)
+			child = child.Index(v)
 		case string:
-			child, err = child.Key(v)
+			child = child.Key(v)
 		}
-		if err != nil {
-			return nil, err
+		if child.HasErrors() {
+			return NewTrowel(nil).addError(child.Errors()...)
 		}
 	}
-	return child, nil
+	return child
 }
 
 func (w *trowelWrapper) Array() ([]interface{}, error) {
 	if w.data == nil {
-		return nil, NewError("underlying data is nil")
+		return nil, NewError(w, "underlying data is nil")
 	}
 	var arr []interface{}
 	var ok bool
 	if arr, ok = w.data.([]interface{}); !ok {
-		return nil, NewError("underlying data is not an array")
+		return nil, NewError(w, "underlying data is not an array")
 	}
 	return arr, nil
 }
 
 func (w *trowelWrapper) Map() (map[string]interface{}, error) {
 	if w.data == nil {
-		return nil, NewError("underlying data is nil")
+		return nil, NewError(w, "underlying data is nil")
 	}
 	var dict map[string]interface{}
 	var ok bool
 	if dict, ok = w.data.(map[string]interface{}); !ok {
-		return nil, NewError("underlying data is not a map")
+		return nil, NewError(w, "underlying data is not a map")
 	}
 	return dict, nil
 }
 
+func (w *trowelWrapper) HasErrors() bool {
+	return len(w.errors) > 0
+}
+func (w *trowelWrapper) Errors() []error {
+	return w.errors
+}
+func (w *trowelWrapper) Error() error {
+	if w.HasErrors() {
+		return w.errors[len(w.errors)-1]
+	}
+	return nil
+}
+func (w *trowelWrapper) addError(err ...error) Trowel {
+	if w.errors == nil {
+		w.errors = []error{}
+	}
+	w.errors = append(w.errors, err...)
+
+	return w
+}
+
 func NewTrowel(data interface{}) Trowel {
 	return &trowelWrapper{
-		data: data,
+		data:   data,
+		errors: make([]error, 0),
 	}
 }
